0b78c36b59c53088aaea38f1111e7c2557acbfd5
JeanC09WTF <gutierrezjean484@gmail.com>
2023-11-04 23:19:28 -0600

    Actualizacion hasta el video 13

--- src/app/cities/cities.page.ts
+++ src/app/cities/cities.page.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cities',
@@ -13,7 +14,7 @@ export class CitiesPage implements OnInit {
   
   cities: any = []; 
 
-    constructor(private router: Router, private http: HttpClient) { }
+    constructor(private router: Router, private http: HttpClient, public toastController: ToastController, public alertController: AlertController) { }
 
   ngOnInit() {
     this.getCities().subscribe(res => {
@@ -30,6 +31,51 @@ export class CitiesPage implements OnInit {
         })
       )
 
+  }
+
+  async presentToast1(){
+    const toast = await this.toastController.create({
+      message:'Lugar Turistico Seleccionado',
+      duration: 2000,
+      position: "bottom"
+    });
+    toast.present()
+  }
+
+  async presentAlert1(){
+    const alert = await this.alertController.create({
+      header: "Borrar Lugar",
+      message: "Se ha borrado el lugar turistico",
+      buttons: ["Listo"]
+    });
+    await alert.present()
+    let result = await alert.onDidDismiss();
+    console.log(result);
+  }
+
+  async presentAlert2() {
+    const alert = await this.alertController.create({
+      header: "Borrar Lugar",
+      message: "¿Estas Seguro?",
+      buttons: [
+       { 
+          text: 'No',
+          handler: () => {
+            console.log("No Cancelar")
+          }
+       },
+       {
+        text: 'Si',
+        handler: () => {
+          console.log("Seleccion Eliminada")
+        }
+
+       }
+      ]
+    });
+    await alert.present()
+    let result = await alert.onDidDismiss();
+    console.log(result);
   }
 
 }
